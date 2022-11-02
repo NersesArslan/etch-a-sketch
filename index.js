8bb647bb2e15d72e822a764d31a9f74f7a8addc5
NersesArslan <108882259+NersesArslan@users.noreply.github.com>
2022-11-02 15:23:14 +0200

    added reset button

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 const container = document.querySelector('.container');
 
-for(let i = 0; i < 16**2; i++){
+const button = document.querySelector('.btn');
+const reset = document.querySelector('.reset');
+
+
+
+button.addEventListener('click', () => {
+  prompt("How many grids do you want?");
+  
+})
+
+
+function gridMaker(num) {
+for(let i = 0; i < num**2; i++){
   const grid = document.createElement('div');
   grid.classList.add('grid');
   container.appendChild(grid);
+  }
 }
+
 
 grid = document.querySelectorAll('div.grid');
 
+let color = [, "#3C9EE7", "#E7993C",
+            "#E73C99", "#3CE746", "#E7993C"];
+
 grid.forEach((div) => {
   div.addEventListener('mouseover', function() {
-    div.style.background = "#3C9EE7";
+    div.style.background = color[Math.floor(Math.random() * color.length)];
   })
-}
-)
+  reset.addEventListener('click', function(e) {
+     div.style.background = 'white';
+  })
+})
+
+
+
+
+
