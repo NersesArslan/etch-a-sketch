df48da3cd5ecc101a03e32e2c37d8172cc1ca113
NersesArslan <108882259+NersesArslan@users.noreply.github.com>
2022-11-02 20:37:18 +0200

    added customizable grid option

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const container = document.querySelector('.container');
 
 const button = document.querySelector('.btn');
@@ -5,34 +6,55 @@ const reset = document.querySelector('.reset');
 
 
 
+
+
 button.addEventListener('click', () => {
-  prompt("How many grids do you want?");
-  
+  changeSize();
 })
 
+function changeSize(){
+  let number = prompt("How many grids do you want?");
+  container.style.gridTemplateRows = `repeat(${number}, 1fr)`;
+  container.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
+  gridMaker(number);
+}
 
-function gridMaker(num) {
-for(let i = 0; i < num**2; i++){
+function gridMaker(size) {
+
+  container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+
+for(let i = 0; i < size*size; i++){
   const grid = document.createElement('div');
   grid.classList.add('grid');
   container.appendChild(grid);
+
+  let color = [, "#3C9EE7", "#E7993C",
+            "#E73C99", "#3CE746", "#E7993C"];
+  grid.addEventListener('mouseover', function() {
+    grid.style.background = color[Math.floor(Math.random() * color.length)];
+  })
+  reset.addEventListener('click', function(e) {
+    grid.style.background = 'white';
+ })
   }
 }
 
+gridMaker(16);
 
-grid = document.querySelectorAll('div.grid');
 
-let color = [, "#3C9EE7", "#E7993C",
-            "#E73C99", "#3CE746", "#E7993C"];
 
-grid.forEach((div) => {
-  div.addEventListener('mouseover', function() {
-    div.style.background = color[Math.floor(Math.random() * color.length)];
-  })
-  reset.addEventListener('click', function(e) {
-     div.style.background = 'white';
-  })
-})
+
+
+
+  
+ 
+
+
+
+
+
+
 
 
 
